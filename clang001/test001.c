df2b394a8f30b548bc732116f8a14dd5970d4303
seonginy <seonginy@gmail.com>
2018-06-13 13:06:19 +0900

    init

---
+++ clang001/test001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("argc : %d \n", argc);
+    printf("argv : %s \n", argv[0]);
+    printf("argv : %s \n", argv[1]);
+
+    return 0;
+}
---
+++ clang001/test002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int i = 5; //정수형 변수
+    float f = 10.5F; //부동소수점형 변수
+    double d = 20.123;//부동소수점형 변수
+    
+    printf("%d, %f, %lf \n", i, f, d);
+
+    return 0;
+}
